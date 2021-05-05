613df084293a5e46f34eea7b92f12f30a0cdffdd
jorgesilva97 <36448@ufp.edu.pt>
2021-05-05 12:47:42 +0100

    ficheiros

--- src/edu/ufp/inf/projeto/main_project/Run.java
+++ src/edu/ufp/inf/projeto/main_project/Run.java
@@ -1,6 +1,7 @@
 package edu.ufp.inf.projeto.main_project;
 
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.RedBlackBST;
 import edu.ufp.inf.projeto.models.GeoCache;
 import edu.ufp.inf.projeto.models.Objeto;
@@ -25,9 +26,53 @@ public class Run
 
 
 
+    }
 
+    /**
+     * funcao que faz lê do ficheiro input toda a informação dele
+     * @param path
+     */
+    public static void loadFromFile(String path){
+        In in = new In(path);
+        String nParticipantes = in.readLine();
 
+        for(int i = 0; i < Integer.parseInt(nParticipantes); i++){
+            //manuel, luis, premium
+            String[] temParticipante = in.readLine().split(",");
+            int id = Integer.parseInt(temParticipante[0]);
+            String nome = temParticipante[1];
+            String tipo = temParticipante[2];
+            switch (tipo){
+                case "premium":
+                    //PremiumParticipante pp = new PremiumParticipante(id,nome);
+                    break;
+                case "basic":
+                    break;
+                case "admin":
+                    break;
+            }
+        }
+        String nRegioes = in.readLine();
+        for(int i = 0; i < Integer.parseInt(nRegioes); i++){
+            String[] temRegioes = in.readLine().split(",");
+            String nomeRegiao =temRegioes[0];
+            String numeroGeoCaches = temRegioes[1];
+            for (int j = 0; j < Integer.parseInt(numeroGeoCaches); j++){
+                String[] geoCaches = in.readLine().split(",");
+                String nomeG = geoCaches[0];
+                String tipoG = geoCaches[1];
+                String x = geoCaches[2];
+                String y = geoCaches[3];
+                String numObjetos = geoCaches[4];
+                ArrayList<Objeto> objetos = new ArrayList<Objeto>();
+                for (int k=5; k < Integer.parseInt(numObjetos);k++){
+                    String objeto = geoCaches[k];
 
+                }
+                //GeoCache geoCache = new GeoCache(j,);
+
+            }
+        }
 
     }
 
